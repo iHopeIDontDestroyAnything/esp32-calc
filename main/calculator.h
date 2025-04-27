513a91e6b7d6ecb2f7170827941c543579ec7625
iHopeIDontDestroyAnything <jakub.urban62@gmai.com>
2025-04-28 00:39:43 +0200

    binary calculator

---
+++ main/calculator.h
@@ -0,0 +1,12 @@
+#ifndef JAJO_CALCULATOR_H_
+#define JAJO_CALCULATOR_H_
+
+struct calculator
+{
+  char                *m_Buffer;
+  unsigned int       m_BuffSize;
+  unsigned int  m_MaxBufferSize;
+};
+
+
+#endif
